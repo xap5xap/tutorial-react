49853f5a12c975037c83266da19489e5361a03ee
Xavier Perez <xap5xap@gmail.com>
2017-05-29 09:32:04 -0500

    Add tic tac toe starter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// Set up your application entry point here...
+// Set up your application entry point here.../* eslint-disable import/default */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+require('./favicon.ico'); // Tell webpack to load favicon.ico
+import './assets/styles.css';
+
+import Game from './components/game';
+
+render(
+    <AppContainer>
+        <Game />
+    </AppContainer>,
+    document.getElementById('app')
+);
